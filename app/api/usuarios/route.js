08d00b692e9ac9fdaf6ba9b0a50c14dd0621efe5
joseAmatto <amattonetto@gmail.com>
2025-04-02 17:11:10 -0300

    json backend

---
+++ app/api/usuarios/route.js
@@ -0,0 +1,25 @@
+export async  function GET(){
+
+    const usuarios = [
+
+        {nome: "josé", idade: 15}, 
+        {nome: "jessica", idade: 22},
+        {nome: "pedro", idade: 36},
+
+    ]
+
+
+
+
+
+    return new Response(
+
+      JSON.stringify (usuarios),
+
+        {
+            status: 200,
+            headers: { "content-type": "application/json" }
+        }
+    )
+
+}
